9576d09a4f11dbbedddef8b9df0076c426f7cef9
Ariachnide <rb.guerre@gmail.com>
2025-11-02 12:17:41 +0100

    count setup

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { useCounterStore } from './stores/counter';
+  const counter = useCounterStore()
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +9,16 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p>
+    test counter: {{ counter.count }}
+  </p>
+  <p>
+    nomber of actions: {{ counter.actionCount }}
+  </p>
+  <button @click="counter.doubleCount()">--- Double ---</button>
+  <button @click="counter.increment()">--- Up ---</button>
+  <button @click="counter.reset()">--- Reset ---</button>
+  <button @click="counter.decrement()">--- Down ---</button>
 </template>
 
 <style scoped></style>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,26 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: 0,
+    actionCount: 0
+  }),
+  actions: {
+    increment() {
+      this.actionCount++
+      this.count++
+    },
+    decrement() {
+      this.actionCount++
+      this.count--
+    },
+    reset() {
+      this.actionCount++
+      this.count = 0
+    },
+    doubleCount() {
+      this.actionCount++
+      this.count = this.count * 2
+    }
   }
-
-  return { count, doubleCount, increment }
 })
